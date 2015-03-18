3216e9d4029e1acd3cff35cb1383589485b9e6ee
Kyle Giard-Chase & james nielson <student@epicodus.com>
2015-03-18 15:51:37 -0700

    Add functionality to cuisine.php to pass find method test

--- src/Cuisine.php
+++ src/Cuisine.php
@@ -55,6 +55,19 @@
             $GLOBALS['DB']->exec("DELETE FROM cuisines *;");
         }
 
+        static function find($search_id)
+        {
+            $found_cuisine = null;
+            $cuisines = Cuisine::getAll();
+            foreach($cuisines as $cuisine){
+                $cuisine_id = $cuisine->getId();
+                if($cuisine_id == $search_id){
+                    $found_cuisine = $cuisine;
+                }
+            }
+            return $found_cuisine;
+        }
+
 
     }
 
