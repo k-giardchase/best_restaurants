b9d4c92dea6e02dd471547c61dc15baccf924f3e
Kyle Giard-Chase & james nielson <student@epicodus.com>
2015-03-18 10:30:56 -0700

    Begin project.

---
+++ src/Cuisine.php
@@ -0,0 +1,46 @@
+<?php
+
+    class Cuisine
+    {
+            private $type;
+            private $id;
+
+        function __construct($type, $id=null)
+        {
+            $this->type = $type;
+            $this->id = $id;
+        }
+
+        function getType()
+        {
+            return $this->type;
+        }
+        function setType($new_type)
+        {
+            $this->type = (string) $new_type;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = (int) $new_id;
+        }
+
+        function save(){
+            $statement = $GLOBALS['DB']->query("INSERT INTO categories (type) VALUES ('{$this->getType()}') RETURNING id;");
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $this->setId($result['id']);
+        }
+
+        static function getAll(){
+            
+        }
+
+
+    }
+
+?>
